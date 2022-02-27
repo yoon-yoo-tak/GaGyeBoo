d798bac321b13bcc5f6b74434b2f615f3a709e38
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-02-27 17:07:00 +0900

    Initial commit

---
+++ src/main/java/com/our/HhaApplication.java
@@ -0,0 +1,13 @@
+package com.our;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhaApplication.class, args);
+	}
+
+}
