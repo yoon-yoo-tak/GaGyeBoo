7b0c93c0094f64922d11e5d21c3e831f3dc7f733
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-03-03 20:45:15 +0900

    2022.03.03
    Entity 클래스 생성
    db 테스트

---
+++ src/main/java/com/our/domain/records/Record.java
@@ -0,0 +1,35 @@
+package com.our.domain.records;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id; // 번호
+
+    private LocalDateTime date; // 날짜
+
+    private int spend; // 사용금액
+
+    private String purpose; // 목적
+
+    private String memo; // 메모
+
+    @Builder
+    public Record(int spend, String purpose, String  memo){
+        this.spend = spend;
+        this.purpose = purpose;
+        this.memo = memo;
+    }
+
+}
---
+++ src/test/java/com/our/domain/recordRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.our.domain;
+
+import com.our.domain.records.Record;
+import com.our.domain.records.recordRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+public class recordRepositoryTest {
+
+    @Autowired
+    recordRepository recordRepository;
+
+    @Test
+    public void save_load(){
+        //given
+        recordRepository.save(Record.builder()
+                .spend(3400)
+                .purpose("asd")
+                .memo("ddd").build());
+
+        //when
+
+        List<Record> recordList = recordRepository.findAll();
+
+        //then
+        Record record = recordList.get(0);
+        assertThat(record.getPurpose(), is("asd"));
+        assertThat(record.getMemo(),is("ddd"));
+
+    }
+
+}
